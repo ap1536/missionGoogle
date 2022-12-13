7bd06e88f5f5901e6d3faac3f28415890dc11e2c
ap1536 <aravind.pabbathi@gmail.com>
2022-12-13 21:38:46 +0530

    CTCI - Chapter1 Implemented 1.1 problem

--- com/msg/ctci/fourthedition/Chapter1.java
+++ com/msg/ctci/fourthedition/Chapter1.java
@@ -1,5 +1,7 @@
 package com.msg.ctci.fourthedition;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -18,17 +20,29 @@ public class Chapter1 {
         return al;
     }
 
-    public String makeSentence(String[] words){
+    public String makeSentence(String @NotNull [] words){
         StringBuilder sentence = new StringBuilder();
         for(String word:words){
             sentence.append(word);
-            boolean flag = checkIfStringHasUniqueChars(word);
+            boolean flag = checkIfStringHasUniqueChars(word.toLowerCase());
+            System.out.println("Word "+word+((flag)?" contains unique letters":" contains duplicate letters"));
         }
         return sentence.toString();
     }
 
+    /**
+     * 1.1 Implement an algorithm to determine if a string has all unique characters.
+     * What if you can not use additional data structures?
+      * @param word is input string
+     * @return boolean value true if word contains unique characters else false
+     */
     public boolean checkIfStringHasUniqueChars(String word) {
-
-        return false;
+        boolean[] char_set = new boolean[256];
+        for(int i=0;i<word.length();i++){
+            int val = word.charAt(i);
+            if(char_set[val]) return false;
+            char_set[val] = true;
+        }
+        return true;
     }
 }
